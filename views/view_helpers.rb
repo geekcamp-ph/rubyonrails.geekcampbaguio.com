947677165d3e8d9a3fe3fccb8125c11d8b5d71c1
George Mendoza <gsmendoza@gmail.com>
2013-08-30 17:02:24 +0800

    Chore: Replace lesson number with id.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,14 +1,14 @@
 module ViewHelpers
-  class Lesson < Struct.new(:id, :category, :title, :basename, :lesson_number)
+  class Lesson < Struct.new(:id, :category, :title, :basename)
     DATA = [
-      new(1, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails', 1),
-      new(2, 'lesson', 'Database CRUD', '20-database-crud', 2),
-      new(3, 'lesson', 'Show Itinerary Page', '30-show', 3),
-      new(4, 'lesson', 'List Itineraries Page', '40-index-page', 4),
-      new(5, 'lesson', 'Create Itinerary Page', '50-create', 5),
-      new(6, 'lesson', 'Update Itinerary Page', '60-update', 6),
-      new(7, 'lesson', 'Delete Itinerary Page', '70-delete', 7),
-      new(8, 'lesson', 'Validate Itinerary', '80-validate', 8),
+      new(1, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails'),
+      new(2, 'lesson', 'Database CRUD', '20-database-crud'),
+      new(3, 'lesson', 'Show Itinerary Page', '30-show'),
+      new(4, 'lesson', 'List Itineraries Page', '40-index-page'),
+      new(5, 'lesson', 'Create Itinerary Page', '50-create'),
+      new(6, 'lesson', 'Update Itinerary Page', '60-update'),
+      new(7, 'lesson', 'Delete Itinerary Page', '70-delete'),
+      new(8, 'lesson', 'Validate Itinerary', '80-validate'),
     ]
 
     def self.find(id)
