a1cd456344ad5d3943162f7f3243ad255e4f37e0
George Mendoza <gsmendoza@gmail.com>
2013-08-30 17:00:22 +0800

    Update table of contents: link to new window.   Set ids to 1 to 8.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,14 +1,14 @@
 module ViewHelpers
   class Lesson < Struct.new(:id, :category, :title, :basename, :lesson_number)
     DATA = [
-      new(:installation, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails', 1),
-      new(:database_crud, 'lesson', 'Database CRUD', '20-database-crud', 2),
-      new(:show_itinerary, 'lesson', 'Show Itinerary Page', '30-show', 3),
-      new(:list_itineraries, 'lesson', 'List Itineraries Page', '40-index-page', 4),
-      new(:create_itinerary, 'lesson', 'Create Itinerary Page', '50-create', 5),
-      new(:update_itinerary, 'lesson', 'Update Itinerary Page', '60-update', 6),
-      new(:delete_itinerary, 'lesson', 'Delete Itinerary Page', '70-delete', 7),
-      new(:validate_itinerary, 'lesson', 'Validate Itinerary', '80-validate', 8),
+      new(1, 'lesson', 'Installing and Running Rails', '10-installing-and-running-rails', 1),
+      new(2, 'lesson', 'Database CRUD', '20-database-crud', 2),
+      new(3, 'lesson', 'Show Itinerary Page', '30-show', 3),
+      new(4, 'lesson', 'List Itineraries Page', '40-index-page', 4),
+      new(5, 'lesson', 'Create Itinerary Page', '50-create', 5),
+      new(6, 'lesson', 'Update Itinerary Page', '60-update', 6),
+      new(7, 'lesson', 'Delete Itinerary Page', '70-delete', 7),
+      new(8, 'lesson', 'Validate Itinerary', '80-validate', 8),
     ]
 
     def self.find(id)
